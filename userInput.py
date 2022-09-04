55ef8a740e6736ae935197b5bcb77f7f4f2a7f34
Suraj Thapa <stsurajst@gmail.com>
2022-09-04 23:10:34 +0545

    learnig python syntax

---
+++ userInput.py
@@ -0,0 +1,4 @@
+name = input("Input your name:")
+age = int(input("Input Your age:"))
+
+print("Your name is:  " + name + "and you are", age)
---
+++ variablesInPython.py
@@ -0,0 +1,10 @@
+# Variable
+
+name = "Suraj"
+print(name)
+
+# Concatination
+age = 27
+
+print(name+ " is from Nepal", age )
+print(len(name))
