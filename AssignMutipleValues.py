226c24f1591200b0e11ce583afdefd071ffbecc3
Suraj Thapa <stsurajst@gmail.com>
2023-01-17 14:27:39 -0600

    variables

---
+++ AssignMutipleValues.py
@@ -0,0 +1,21 @@
+# Many values to Multiple Variable
+
+x, y, z = "Orange", "Banana", "Cherry"
+
+print(x)
+print(y)
+print(z)
+
+# One value to multiple variable
+x = y = z = "Orange"
+
+#Unpack a Collection 
+# Unpack a list
+
+fruits = ["apple", "banana", "cherry"]
+
+a, b, c = fruits
+
+print(a)
+print(b)
+print(c)
